078a59bbce3c8208de815bf0eb9a610d83aeac93
GalaxyAstronaut <37252186+GalaxyAstronaut@users.noreply.github.com>
2020-03-29 23:13:33 -0400

    Add files via upload

---
+++ turtledemo.py
@@ -0,0 +1,11 @@
+import turtle
+# from turtle import * / it's frowned upon
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+turtle.right(350)
+turtle.right(350)
+turtle.forward(250)
+
+turtle.done()
